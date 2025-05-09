a04225805786469d162be05cbca02ea4ca868653
sa-savi <savi.toy@amkcambodia.com>
2025-05-09 08:39:57 +0700

    update

--- mount-dfs/scripts/service_file.sh
+++ mount-dfs/scripts/service_file.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-# MOUNT_SCRIPT="/usr/local/bin/mount-amkdfs.sh"
-# UMOUNT_SCRIPT="/usr/local/bin/unmount-amkdfs.sh"
-# SERVICE_FILE="/etc/systemd/system/mount-amkdfs.service"
+# !/bin/bash
+MOUNT_SCRIPT="/usr/local/bin/mount-amkdfs.sh"
+UMOUNT_SCRIPT="/usr/local/bin/unmount-amkdfs.sh"
+SERVICE_FILE="/etc/systemd/system/mount-amkdfs.service"
 
-source .env/services_file
-source .env/umount_script
-source .env/mount_script
+# source .env/services_file
+# source .env/umount_script
+# source .env/mount_script
 
 sudo tee "$SERVICE_FILE" > /dev/null <<EOF
 [Unit]
